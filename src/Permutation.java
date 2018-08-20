77b672761af9d9a734f9c6f4432246122d045efd
Kyle Marrero <kylemarrero@Kyles-MacBook-Pro.local>
2018-08-20 01:19:10 -0500

    randomized queue

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,17 +1,11 @@
 public class Permutation {
-    static Deque<String> deque = new Deque<>();
+    static RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
 
     public static void main(String[] args) {
-        deque.addFirst("first");
-       deque.addFirst("first2");
-       deque.addLast("Last");
-//        deque.addLast("LASST");
-        //deque.addLast("last");
-        //deque.removeLast();
-        deque.removeFirst();
-        //deque.removeFirst();
-        for (String s: deque){
-            System.out.println(s);
-        }
+        //randomizedQueue.enqueue("hey");
+        randomizedQueue.enqueue("hey2");
+
+            System.out.println(randomizedQueue.sample());
+
     }
 }
--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,12 +1,28 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    int n;
+    private int n;
+    private Item[] a;
 
     public RandomizedQueue() {
+        a = (Item[]) new Object[2];
+        n = 0;
+    }
 
+    private void resize(int capacity) {
+        assert capacity >= n;
+
+        // textbook implementation
+//        Item[] temp = (Item[]) new Object[capacity];
+//        for (int i = 0; i < n; i++) {
+//            temp[i] = a[i];
+//        }
+//        a = temp;
+        a = java.util.Arrays.copyOf(a, capacity);
     }
 
     public boolean isEmpty() {
@@ -18,28 +34,58 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public void enqueue(Item item) {
+        n++;
+        if (n == a.length) resize(2*a.length);
+        a[n++] = item;
 
+    }
+
+    public int randomIndex() {
+        return StdRandom.uniform(n-1);
     }
 
     public Item dequeue() {
-        return;
+        int i = randomIndex();
+        Item j = a[i];
+        a[i] = a[n-1];
+        a[n-1] = null;
+        n--;
+        if (n > 0 && n == a.length/4) resize(a.length/2);
+        return j;
     }
 
     public Item sample() {
-        return;
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        return a[randomIndex()];
     }
 
     @Override
     public Iterator<Item> iterator() {
-        return RandomizedIterator();
+        return new RandomizedIterator();
     }
 
     private class RandomizedIterator implements Iterator<Item> {
 
-        public Item next(){
-            if(!hasNext()) {throw new NoSuchElementException();}
-            Item item = new Item();
-            return item;
+        private Item[] j;
+        private int i;
+
+        public RandomizedIterator() {
+            copy();
+            StdRandom.shuffle(a);
+        }
+        private void copy() {
+            j = (Item[]) new Object[n];
+
+            for(int i =0; i<n; i++) {
+                j[i] = a[i];
+            }
+        }
+        public boolean hasNext()  { return i < n-1; }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return j[i++];
         }
     }
 }
